6719713fae9134ad13d604ebec630e21a93dce2a
ledochowitsch <ledochowitsch@users.noreply.github.com>
2018-10-25 14:54:47 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from setuptools import setup, find_packages
 setup(
     name='bayesian-optimization',
     version='0.1.0',
-    url='https://github.com/ledochowitsch/Neural_Decoding.git',
+    url='https://github.com/ledochowitsch/Neural_Decoding/',
     packages=find_packages(),
     author='Kording_Lab',
     author_email="kording@gmail.com",
     description='NeuralDecoder',
-    download_url='https://github.com/ledochowitsch/Neural_Decoding.git,
+    download_url='https://github.com/ledochowitsch/Neural_Decoding/',
     install_requires=[
     ],
 )
